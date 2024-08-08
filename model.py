d17a49dc12e7ae954e354f869d31d2267d85f863
김민규 <km0616k@gmail.com>
2024-08-08 17:24:37 +0900

    Add validation

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from torch import optim
-from torch.nn.functional import binary_cross_entropy
 
 import layer
 from function import complex_mse_loss
@@ -116,22 +115,34 @@ class DCUnet(nn.Module):
 
         return output
 
-    def train_epoch(self, batches):
-        loss = {'loss': 0.0}
+    def train_epoch(self, train_loader, validation_loader):
+        loss = {'loss': 0.0, 'val_loss': 0.0}
         epoch_loss = 0.0
-        for features_batch, labels_batch in batches:
+        for input_batch, target_batch in train_loader:
             self.train()
 
-            input = features_batch
-            label = labels_batch
-            output = self(input)
-            loss = complex_mse_loss(output, label)
+            output = self(input_batch)
+            loss = complex_mse_loss(output, target_batch)
             epoch_loss += loss.item()
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
 
         loss['loss'] = epoch_loss
+
+        if validation_loader is not None:
+            val_loss = 0.0
+            for input_batch, target_batch in train_loader:
+                self.eval()
+
+                with torch.no_grad:
+                    output = self(input_batch)
+                    loss = complex_mse_loss(output, target_batch)
+                    val_loss += loss.item()
+
+            loss['val_loss'] = val_loss
+
+        return loss
 
     def save(self, name, min, max):
         torch.save({
@@ -156,14 +167,14 @@ class DAAE(nn.Module):
         self.optimizer_autoencoder = optim.RMSprop(self.autoencoder.parameters())
         self.optimizer_discriminator = optim.RMSprop(self.discriminator.parameters())
 
-    def train_epoch(self, batches):
+    def train_epoch(self, train_loader, validation_loader):
         loss = {'generator': 0.0, 'reconstruction': 0.0, 'discriminator': 0.0}
         epoch_loss_generator = 0.0
         epoch_loss_reconstruction = 0.0
         epoch_loss_discriminator = 0.0
 
         b = 0
-        for features_batch, labels_batch in batches:
+        for features_batch, labels_batch in train_loader:
             self.autoencoder.train()
             self.discriminator.train()
 
@@ -225,29 +236,63 @@ class DAAE(nn.Module):
         def __init__(self):
             super().__init__()
 
-            self.conv1 = layer.ComplexConv2d(in_channels=1, out_channels=16, kernel_size=5, padding=2, stride=2)
+            self.conv1 = layer.ComplexConv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
             self.batch1 = layer.ComplexBatchNorm2d(num_features=16)
             self.activation1 = layer.ComplexLeakyReLU()
-            self.conv2 = layer.ComplexConv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2, stride=2)
+
+            self.conv2 = layer.ComplexConv2d(in_channels=16, out_channels=32, kernel_size=4, padding=1, stride=2)
             self.batch2 = layer.ComplexBatchNorm2d(num_features=32)
             self.activation2 = layer.ComplexLeakyReLU()
-            self.conv3 = layer.ComplexConv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2, stride=2)
+
+            self.conv3 = layer.ComplexConv2d(in_channels=32, out_channels=64, kernel_size=4, padding=1, stride=2)
             self.batch3 = layer.ComplexBatchNorm2d(num_features=64)
             self.activation3 = layer.ComplexLeakyReLU()
-            self.conv4 = layer.ComplexConv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2, stride=2)
+
+            self.conv4 = layer.ComplexConv2d(in_channels=64, out_channels=128, kernel_size=4, padding=1, stride=2)
             self.batch4 = layer.ComplexBatchNorm2d(num_features=128)
             self.activation4 = layer.ComplexLeakyReLU()
 
-            self.conv5 = layer.ComplexConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, padding=1, stride=2, output_padding=1)
-            self.batch5 = layer.ComplexBatchNorm2d(num_features=64)
+            self.conv5 = layer.ComplexConv2d(in_channels=128, out_channels=256, kernel_size=4, padding=1, stride=2)
+            self.batch5 = layer.ComplexBatchNorm2d(256)
             self.activation5 = layer.ComplexLeakyReLU()
-            self.conv6 = layer.ComplexConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, padding=1, stride=2, output_padding=1)
-            self.batch6 = layer.ComplexBatchNorm2d(num_features=32)
+
+            self.conv6 = layer.ComplexConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=1)
+            self.batch6 = layer.ComplexBatchNorm2d(256)
             self.activation6 = layer.ComplexLeakyReLU()
-            self.conv7 = layer.ComplexConvTranspose2d(in_channels=32, out_channels=16, kernel_size=3, padding=1, stride=2, output_padding=1)
-            self.batch7 = layer.ComplexBatchNorm2d(num_features=16)
+
+            self.conv7 = layer.ComplexConvTranspose2d(in_channels=512, out_channels=128, kernel_size=4, padding=1, stride=2)
+            self.batch7 = layer.ComplexBatchNorm2d(128)
             self.activation7 = layer.ComplexLeakyReLU()
-            self.conv8 = layer.ComplexConvTranspose2d(in_channels=16, out_channels=1, kernel_size=3, padding=1, stride=2, output_padding=1)
+
+            self.conv8 = layer.ComplexConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, padding=1, stride=1)
+            self.batch8 = layer.ComplexBatchNorm2d(64)
+            self.activation8 = layer.ComplexLeakyReLU()
+
+            self.conv9 = layer.ComplexConvTranspose2d(in_channels=192, out_channels=64, kernel_size=4, padding=1, stride=2)
+            self.batch9 = layer.ComplexBatchNorm2d(64)
+            self.activation9 = layer.ComplexLeakyReLU()
+
+            self.conv10 = layer.ComplexConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, padding=1, stride=1)
+            self.batch10 = layer.ComplexBatchNorm2d(32)
+            self.activation10 = layer.ComplexLeakyReLU()
+
+            self.conv11 = layer.ComplexConvTranspose2d(in_channels=96, out_channels=32, kernel_size=4, padding=1, stride=2)
+            self.batch11 = layer.ComplexBatchNorm2d(32)
+            self.activation11 = layer.ComplexLeakyReLU()
+
+            self.conv12 = layer.ComplexConvTranspose2d(in_channels=32, out_channels=16, kernel_size=3, padding=1, stride=1)
+            self.batch12 = layer.ComplexBatchNorm2d(16)
+            self.activation12 = layer.ComplexLeakyReLU()
+
+            self.conv13 = layer.ComplexConvTranspose2d(in_channels=48, out_channels=16, kernel_size=4, padding=1, stride=2)
+            self.batch13 = layer.ComplexBatchNorm2d(16)
+            self.activation13 = layer.ComplexLeakyReLU()
+
+            self.conv14 = layer.ComplexConvTranspose2d(in_channels=16, out_channels=2, kernel_size=3, padding=1, stride=1)
+            self.batch14 = layer.ComplexBatchNorm2d(2)
+            self.activation14 = layer.ComplexLeakyReLU()
+
+            self.conv15 = layer.ComplexConvTranspose2d(in_channels=2, out_channels=1, kernel_size=3, padding=1, stride=1)
 
         def encode(self, x):
             x = self.activation1(self.batch1(self.conv1(x)))
